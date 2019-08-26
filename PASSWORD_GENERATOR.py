1305f257c164e4f80c77c2e56f43fcd30b2b63ea
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-08-27 03:47:42 +0530

    Delete PASSWORD_GENERATOR.py

--- PASSWORD_GENERATOR.py
+++
@@ -1,8 +0,0 @@
-import random
-import pyperclip
-chars = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()'
-password = ''
-for c in range(6):
-   password += random.choice(chars)
-print('The Generated Password is %r and it has been copied to your clipboard' %(password))
-pyperclip.copy(password)
